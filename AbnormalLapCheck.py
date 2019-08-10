66b0bd6eb6faa602337fcb97452f70c89653ea9e
Ben <benblas94@gmail.com>
2019-08-10 08:05:55 -0700

    Bug fixes on AbnormalLapCheck

--- AbnormalLapCheck.py
+++ AbnormalLapCheck.py
@@ -1,9 +1,11 @@
+import time
+
 from Parser import get_leader_board, get_race_data
 from Util import calc_millisec
 
 # WIP
 
-refreshrate = 1
+refreshrate = 3
 
 
 class TeamLapCheck:
@@ -17,8 +19,7 @@ class TeamLapCheck:
         self.last_time = self.initial_leader_board[reg_num]['last_time']
         self.total_time = self.initial_leader_board[reg_num]['total_time']
         self.race_time = self.initial_race_data['racetime']
-        self.long_lap_triggered = False
-        self.over_double_lap_triggered = False
+        self.msg_triggered = False
         self.drop_out_triggered = False
 
     def refresh_info(self):
@@ -30,23 +31,29 @@ class TeamLapCheck:
         self.race_time = get_race_data()['racetime']
 
     def long_lap(self):
-        self.long_lap_triggered = True
+        self.msg_triggered = True
 
     def over_double_lap(self):
-        self.over_double_lap_triggered = True
+        self.msg_triggered = True
 
     def drop_out(self):
         self.drop_out_triggered = True
+        self.msg_triggered = True
 
     def normal_lap(self):
-        self.long_lap_triggered = False
-        self.over_double_lap_triggered = False
+        self.msg_triggered = False
         self.drop_out_triggered = False
 
     def drop_out_check(self):
         race_time = calc_millisec(self.race_time)
         total_time = calc_millisec(self.total_time)
-        if race_time - total_time > 60 * 1000:  # 10 min
+        if race_time - total_time > 10 * 60 * 1000:  # 10 min
+            return True
+        else:
+            return False
+
+    def normal_lap_check(self):
+        if abs(calc_millisec(self.last_time) - calc_millisec(self.avg_lap_time)) < 30 * 1000:  # 30 secs
             return True
         else:
             return False
@@ -56,17 +63,18 @@ class TeamLapCheck:
         avg_time_ms = calc_millisec(self.avg_lap_time)
         is_1min_over = avg_time_ms <= (last_time - 60000)
         is_over_double = avg_time_ms * 2 <= last_time
-        if is_1min_over and not is_over_double:
+        not_triggered = not self.msg_triggered and not self.drop_out_triggered
+        if is_1min_over and not is_over_double and not_triggered:
             self.long_lap()
             print('Car {carnum} has a long lap at {time}'.format(carnum=self.car_num, time=self.total_time))
-        elif is_over_double:
+        elif is_over_double and not_triggered:
             self.over_double_lap()
             print('Car {carnum} might have missed a lap at {time}'.format(carnum=self.car_num, time=self.total_time))
         elif self.drop_out_check() and not self.drop_out_triggered:
             self.drop_out()
             print('Car {carnum} is not hitting, last crossing at {time}'.format(carnum=self.car_num,
                                                                                 time=self.total_time))
-        else:
+        elif self.normal_lap_check():
             self.normal_lap()
 
 
@@ -94,15 +102,17 @@ def start_abnormal_lap_check():
         if len(team_obj_dict) < len(leader_board):
             new_driver = add_driver(team_obj_dict, leader_board)
             team_obj_dict[new_driver.reg_num] = new_driver
+            print('add driver triggered')
 
         for driver_key in team_obj_dict:
             driver = team_obj_dict[driver_key]
             if driver.reg_num not in leader_board:
                 instantiate_team_lap_check()
+                print('Reinstantiating TeamLapCheck')
                 break
             if get_leader_board()[driver.reg_num]['last_time'] != 'IN PIT' and get_leader_board()[driver.reg_num]['last_time'] != '':
                 new_lap_time = calc_millisec(get_leader_board()[driver.reg_num]['last_time'])
                 driver.check_time(new_lap_time)
                 driver.refresh_info()
+        time.sleep(refreshrate)
 start_abnormal_lap_check()
- #  why is this repeating msgs?
